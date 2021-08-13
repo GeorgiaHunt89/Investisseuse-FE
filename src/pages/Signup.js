bee0836e612dd109784b587c97e8cad580f22d8e
Georgia Hunt <georgialhunt89@gmail.com>
2021-08-13 16:12:25 +0930

    Added additional login variables for mutation

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -11,7 +11,13 @@ function Signup(props) {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     const mutationResponse = await addUser({
-      variables: { ...formState },
+      variables: {
+        email: formState.email,
+        password: formState.password,
+        firstName: formState.firstName,
+        lastName: formState.lastName,
+        role: formState.role,
+      },
     });
     const token = mutationResponse.data.addUser.token;
     Auth.login(token);
