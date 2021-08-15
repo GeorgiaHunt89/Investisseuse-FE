b2bc57072d0dae7936114ca7f755a96b0620d232
Georgia Hunt <georgialhunt89@gmail.com>
2021-08-15 10:55:11 +0930

    Added new styling to Signup page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -3,21 +3,45 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../state/auth';
 import { ADD_USER } from '../api/mutations';
+import { Form } from 'semantic-ui-react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import { MemoryRouter as Router } from 'react-router';
+import { Link as RouterLink } from 'react-router-dom';
+
+import NavBar from '../components/navigation/NavBar';
+import AppForm from '../components/form/AppForm';
+import { email, required } from '../components/form/validate';
+import FormButton from '../components/button/FormButton';
+import Button from '../components/button/button';
+import FormFeedback from '../components/form/FormFeedback';
+import { Field, FormSpy } from 'react-final-form';
+import Typography from '../components/typography/Typography';
+import InputTextField from '../components/form/InputTextField';
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    marginTop: theme.spacing(6),
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(2),
+  },
+  feedback: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 function Signup(props) {
+  const classes = useStyles();
+  const [sent, setSent] = React.useState(false);
   const [formState, setFormState] = useState({ email: '', password: '', firstName: '', lastName: '', role: '' });
   const [addUser] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     const mutationResponse = await addUser({
-      variables: {
-        email: formState.email,
-        password: formState.password,
-        firstName: formState.firstName,
-        lastName: formState.lastName,
-        role: formState.role,
-      },
+      variables: { ...formState },
     });
     const token = mutationResponse.data.addUser.token;
     Auth.login(token);
@@ -31,37 +55,111 @@ function Signup(props) {
     });
   };
 
-  return (
-    <div className="container my-1">
-      <Link to="/login">← Go to Login</Link>
+  const role = [
+    { key: 'Founder', text: 'Founder', value: 'founder' },
+    { key: 'Investor', text: 'Investor', value: 'investor' },
+  ];
 
-      <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
-          <input placeholder="First" name="firstName" type="firstName" id="firstName" onChange={handleChange} />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input placeholder="Last" name="lastName" type="lastName" id="lastName" onChange={handleChange} />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input placeholder="youremail@test.com" name="email" type="email" id="email" onChange={handleChange} />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input placeholder="******" name="password" type="password" id="pwd" onChange={handleChange} />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="role">Role:</label>
-          <input placeholder="founder or investor" name="role" type="role" id="role" onChange={handleChange} />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+  return (
+    <React.Fragment>
+      <NavBar />
+      <AppForm>
+        <React.Fragment>
+          <Typography variant="h3" gutterBottom marked="center" align="center">
+            Sign Up
+          </Typography>
+          <Typography variant="body2" align="center">
+            <Link href="/Login" align="center" underline="always">
+              Already have an account?
+            </Link>
+          </Typography>
+        </React.Fragment>
+        <Form.Group Submit={handleFormSubmit} widths="equal">
+          <Grid container spacing={5}>
+            <Grid item xs={18} sm={12}>
+              <Form.Input
+                fluid
+                label="First name"
+                placeholder="First name"
+                type="firstName"
+                id="firstName"
+                onChange={handleChange}
+                component={InputTextField}
+              />
+            </Grid>
+          </Grid>
+          <Grid container spacing={5}>
+            <Grid item xs={18} sm={12}>
+              <Form.Input
+                fluid
+                label="Last name"
+                placeholder="Last name"
+                type="lastName"
+                id="lastName"
+                onChange={handleChange}
+                component={InputTextField}
+              />
+            </Grid>
+          </Grid>
+        </Form.Group>
+        <Grid container spacing={5}>
+          <Grid item xs={18} sm={12}>
+            <Form.Group widths="equal">
+              <Form.Input fluid label="Password" placeholder="*****" type="password" id="pwd" onChange={handleChange} />
+              <Form.Input
+                fluid
+                label="Confirm Password"
+                placeholder="*****"
+                type="confirmPassword"
+                id="confirmPwd"
+                onChange={handleChange}
+                component={InputTextField}
+              />
+            </Form.Group>
+          </Grid>
+        </Grid>
+        <Grid container spacing={5}>
+          <Grid item xs={18} sm={12}>
+            <Form.Group widths="equal">
+              <Form.Input
+                fluid
+                label="Email Address"
+                placeholder="youremail@address.com"
+                type="email"
+                id="email"
+                onChange={handleChange}
+                component={InputTextField}
+              />
+            </Form.Group>
+          </Grid>
+        </Grid>
+        <Grid container spacing={5}>
+          <Grid item xs={18} sm={12}>
+            <Form.Select
+              fluid
+              label="Please select either Investor or Founder"
+              options={role}
+              placeholder="Investor / Founder"
+              type="role"
+              id=""
+              onChange={handleChange}
+              component={InputTextField}
+            />
+          </Grid>
+        </Grid>
+
+        <FormButton
+          type="submit"
+          className={classes.button}
+          color="secondary"
+          component={RouterLink}
+          to="/login"
+          fullWidth
+        >
+          {setFormState || formState ? 'In progress…' : 'Sign Up'}
+        </FormButton>
+      </AppForm>
+    </React.Fragment>
   );
 }
 
