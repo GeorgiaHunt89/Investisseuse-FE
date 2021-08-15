e552e91416350471a84e93d92f64daa9d9fe52f1
Georgia Hunt <georgialhunt89@gmail.com>
2021-08-15 22:19:24 +0930

    Created Investor Dashboard

---
+++ src/pages/investors/InvestorsDashboard.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import { Box, Grid, Container, Typography } from '@material-ui/core';
+// components
+// import Page from '../components/page/Page';
+import CreateNewBusiness from '../../components/dashboard/founder/CreateNewBusiness';
+import BusinessPage from '../../components/dashboard/founder/BusinessPage';
+import Messages from '../../components/dashboard/founder/Messages';
+import NewInvestors from '../../components/dashboard/founder/NewInvestors';
+import Reports from '../../components/dashboard/founder/Reports';
+import BusinessPageVisits from '../../components/dashboard/founder/BusinessPageVisits';
+// import BlogPosts from '../components/dashboard/founder/BlogPosts';
+// import TrafficBySites from '../components/dashboard/founder/TrafficBySites';
+// import Tasks from '../components/dashboard/founder/Tasks';
+
+// ----------------------------------------------------------------------
+
+export default function FounderDashboard() {
+  return (
+    // <Page title="Founder Dashboard">
+    <Container maxWidth="xl">
+      <Box sx={{ pb: 5 }}>
+        <Typography variant="h4">Hi, Welcome back</Typography>
+      </Box>
+      <Grid item xs={12} md={6} lg={8}>
+        <CreateNewBusiness />
+      </Grid>
+      <Typography align="center" variant="h4">
+        Business Stats
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6} md={3}>
+          <BusinessPage />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <NewInvestors />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Messages />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Reports />
+        </Grid>
+
+        <Grid item xs={12} md={6} lg={8}>
+          <BusinessPageVisits />
+        </Grid>
+
+        {/* <Grid item xs={12} md={6} lg={8}>
+          <BlogPosts />
+        </Grid> */}
+
+        {/* <Grid item xs={12} md={6} lg={4}>
+          <TrafficBySites />
+        </Grid> */}
+
+        {/* <Grid item xs={12} md={6} lg={8}>
+          <Tasks />
+        </Grid> */}
+      </Grid>
+    </Container>
+    // </Page>
+  );
+}
