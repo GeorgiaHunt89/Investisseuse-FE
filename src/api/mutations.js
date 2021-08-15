f22e61c2a1e8511b0263192292dcd3a1900e1f2e
Georgia Hunt <georgialhunt89@gmail.com>
2021-08-15 17:26:56 +0930

    Updated mutations from BE

--- src/api/mutations.js
+++ src/api/mutations.js
@@ -21,3 +21,57 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const UPDATE_USER = gql`
+  mutation updateUser($firstName: String!, $lastName: String!, $email: String!, $password: String!, $role: String!) {
+    addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password, role: $role) {
+      token
+      user {
+        _id
+        firstName
+        lastName
+        email
+        role
+      }
+    }
+  }
+`;
+
+export const ADD_BUSINESS = gql`
+  mutation addBusiness(
+    $companyName: String!
+    $description: String!
+    $website: String!
+    $logo: String!
+    $sharePrice: Int!
+    $shareQuantity: Int!
+    $pitchDeck: String!
+    $category: String!
+    $owner: User!
+  ) {
+    addBusiness(
+      companyName: $companyName
+      description: $description
+      website: $website
+      logo: $logo
+      sharePrice: $sharePrice
+      shareQuantity: $shareQuantity
+      pitchDeck: $pitchDeck
+      category: $category
+    ) {
+      _id
+      companyName
+      description
+      website
+      logo
+      sharePrice
+      shareQuantity
+      pitchDeck
+      category
+      owner {
+        _id
+        firstName
+      }
+    }
+  }
+`;
