5f4d666717f63f362e91909a6e47695e1076e81f
Georgia Hunt <georgialhunt89@gmail.com>
2021-08-15 17:26:09 +0930

    Added new page routes

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/navigation/NavBar';
 import { StoreProvider } from './state/GlobalState';
-import Profile from './pages/Profile';
+import BusinessProfile from './pages/BusinessProfile';
+import NewBusinessProfile from './pages/NewBusinessProfile';
 // import Investors from './pages/Investors';
-import founder from './pages/Founders';
+import FoundersDashboard from './pages/FoundersDashboard';
 import About from './pages/About';
 import Blog from './pages/Blog';
 
@@ -48,9 +49,10 @@ function App() {
               <Route exact path="/" component={Home} />
               <Route exact path="/Login" component={Login} />
               <Route exact path="/Signup" component={Signup} />
-              <Route exact path="/profile" component={Profile} />
+              <Route exact path="/BusinessProfile" component={BusinessProfile} />
+              <Route exact path="/NewBusinessProfile" component={NewBusinessProfile} />
               {/* <Route exact path="/Investors" component={requireAuth(Investors)} /> */}
-              <Route exact path="/Founders" component={requireAuth(founder)} />
+              <Route exact path="/FoundersDashboard" component={FoundersDashboard} />
               <Route exact path="/About" component={About} />
               <Route exact path="/Blog" component={Blog} />
               <Route component={NoMatch} />
