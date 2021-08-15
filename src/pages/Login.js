ae1b47e9642fbf7b26dd36c766c47c8d026494f4
Georgia Hunt <georgialhunt89@gmail.com>
2021-08-15 11:02:32 +0930

    Updated login routes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,60 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
-import { LOGIN } from '../api/mutations';
+import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
 import Auth from '../state/auth';
+import { LOGIN } from '../api/mutations';
+// import { Form } from 'semantic-ui-react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+import NavBar from '../components/navigation/NavBar';
+import AppForm from '../components/form/AppForm';
+import { email, required } from '../components/form/validate';
+import FormButton from '../components/button/FormButton';
+import FormFeedback from '../components/form/FormFeedback';
+import { Form, Field, FormSpy } from 'react-final-form';
+import Typography from '../components/typography/Typography';
+import InputTextField from '../components/form/InputTextField';
+// import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    marginTop: theme.spacing(6),
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(2),
+  },
+  feedback: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+// function Login() {
+//   const classes = useStyles();
+//   const [sent, setSent] = React.useState(false);
+
+//   const validate = (values) => {
+//     const errors = required(['email', 'password'], values);
+
+//     if (!errors.email) {
+//       const emailError = email(values.email, values);
+//       if (emailError) {
+//         errors.email = email(values.email, values);
+//       }
+//     }
+
+//     return errors;
+//   };
+
+//   const handleSubmit = () => {
+//     setSent(true);
+//   };
 
 function Login() {
+  const classes = useStyles();
+  const [sent, setSent] = React.useState(false);
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN);
 
@@ -30,29 +80,75 @@ function Login() {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
-
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input placeholder="youremail@test.com" name="email" type="email" id="email" onChange={handleChange} />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input placeholder="******" name="password" type="password" id="pwd" onChange={handleChange} />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <AppForm>
+        <React.Fragment>
+          <Typography variant="h3" gutterBottom marked="center" align="center">
+            Sign In
+          </Typography>
+          <Typography variant="body2" align="center">
+            {'Not a member yet? '}
+            <Link href="/SignUp" align="center" underline="always">
+              Sign Up here
+            </Link>
+          </Typography>
+        </React.Fragment>
+        <Form onSubmit={handleFormSubmit} subscription={{ submitting: true }} onChange={handleChange}>
+          {({ handleSubmit2, submitting }) => (
+            <form onSubmit={handleSubmit2} className={classes.form} noValidate>
+              <Field
+                autoComplete="email"
+                autoFocus
+                component={InputTextField}
+                disabled={submitting || sent}
+                fullWidth
+                label="Email"
+                margin="normal"
+                name="email"
+                required
+                size="large"
+              />
+              <Field
+                fullWidth
+                size="large"
+                component={InputTextField}
+                disabled={submitting || sent}
+                required
+                name="password"
+                autoComplete="current-password"
+                label="Password"
+                type="password"
+                margin="normal"
+              />
+              <FormSpy subscription={{ submitError: true }}>
+                {({ submitError }) =>
+                  submitError ? (
+                    <FormFeedback className={classes.feedback} error>
+                      {submitError}
+                    </FormFeedback>
+                  ) : null
+                }
+              </FormSpy>
+              <FormButton
+                className={classes.button}
+                disabled={submitting || sent}
+                size="large"
+                color="secondary"
+                fullWidth
+              >
+                {submitting || sent ? 'In progress…' : 'Sign In'}
+              </FormButton>
+            </form>
+          )}
+        </Form>
+        <Typography align="center">
+          <Link underline="always" href="/forgot-password/">
+            Forgot password?
+          </Link>
+        </Typography>
+      </AppForm>
+    </React.Fragment>
   );
 }
 
