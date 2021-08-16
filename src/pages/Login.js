de11012db328a51d062e0e411d0237381c3e3bc7
Georgia Hunt <georgialhunt89@gmail.com>
2021-08-16 12:31:54 +0930

    Cleaned up and styled login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,56 +1,70 @@
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
-import Auth from '../state/auth';
-import { LOGIN } from '../api/mutations';
-// import { Form } from 'semantic-ui-react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 
+// ----------------------------------------------------------------------
+
+// Imports from Material Ui
+import { Form, Field, FormSpy } from 'react-final-form';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+import { Container } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
+// import IconButton from 'material-ui/IconButton';
+// import ActionHome from 'material-ui/svg-icons/action/home';
+
+// ----------------------------------------------------------------------
+
+// Components for this page
+import { LOGIN } from '../api/mutations';
+import Auth from '../state/auth';
 import NavBar from '../components/navigation/NavBar';
 import AppForm from '../components/form/AppForm';
 import { email, required } from '../components/form/validate';
 import FormButton from '../components/button/FormButton';
 import FormFeedback from '../components/form/FormFeedback';
-import { Form, Field, FormSpy } from 'react-final-form';
 import Typography from '../components/typography/Typography';
 import InputTextField from '../components/form/InputTextField';
-// import Link from '@material-ui/core/Link';
+import { styles } from '../components/navigation/Toolbar';
+
+// ----------------------------------------------------------------------
+
+// Styling
+const LoginBackgroundImg = '../../img/Login-Background-Img.png';
 
 const useStyles = makeStyles((theme) => ({
+  descriptionCard: {
+    backgroundImage: `url(${LoginBackgroundImg})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  },
   form: {
     marginTop: theme.spacing(6),
+    backgroundColor: '#D9B59D', // Beige
+    opacity: 0.95,
+    backgroundPosition: 'center',
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    color: 'white',
   },
   button: {
+    backgroundColor: '#2B3D5B', // Navy
     marginTop: theme.spacing(3),
     marginBottom: theme.spacing(2),
+    color: 'white',
   },
+
   feedback: {
     marginTop: theme.spacing(2),
   },
 }));
 
-// function Login() {
-//   const classes = useStyles();
-//   const [sent, setSent] = React.useState(false);
+// ----------------------------------------------------------------------
 
-//   const validate = (values) => {
-//     const errors = required(['email', 'password'], values);
-
-//     if (!errors.email) {
-//       const emailError = email(values.email, values);
-//       if (emailError) {
-//         errors.email = email(values.email, values);
-//       }
-//     }
-
-//     return errors;
-//   };
-
-//   const handleSubmit = () => {
-//     setSent(true);
-//   };
+// Login Function
 
 function Login() {
   const classes = useStyles();
@@ -83,70 +97,82 @@ function Login() {
     <React.Fragment>
       <NavBar />
       <AppForm>
-        <React.Fragment>
-          <Typography variant="h3" gutterBottom marked="center" align="center">
-            Sign In
-          </Typography>
-          <Typography variant="body2" align="center">
-            {'Not a member yet? '}
-            <Link href="/SignUp" align="center" underline="always">
-              Sign Up here
-            </Link>
-          </Typography>
-        </React.Fragment>
-        <Form onSubmit={handleFormSubmit} subscription={{ submitting: true }} onChange={handleChange}>
-          {({ handleSubmit2, submitting }) => (
-            <form onSubmit={handleSubmit2} className={classes.form} noValidate>
-              <Field
-                autoComplete="email"
-                autoFocus
-                component={InputTextField}
-                disabled={submitting || sent}
-                fullWidth
-                label="Email"
-                margin="normal"
-                name="email"
-                required
-                size="large"
-              />
-              <Field
-                fullWidth
-                size="large"
-                component={InputTextField}
-                disabled={submitting || sent}
-                required
-                name="password"
-                autoComplete="current-password"
-                label="Password"
-                type="password"
-                margin="normal"
-              />
-              <FormSpy subscription={{ submitError: true }}>
-                {({ submitError }) =>
-                  submitError ? (
-                    <FormFeedback className={classes.feedback} error>
-                      {submitError}
-                    </FormFeedback>
-                  ) : null
-                }
-              </FormSpy>
-              <FormButton
-                className={classes.button}
-                disabled={submitting || sent}
-                size="large"
-                color="secondary"
-                fullWidth
+        <div className={classes.descriptionCard}>
+          <React.Fragment>
+            <Typography variant="h3" gutterBottom align="center">
+              Sign In
+            </Typography>
+            <Typography variant="body2" align="center">
+              {'Not a member yet? '}
+              <Link style={{ color: '#Deba54' }} component={RouterLink} to="/Signup" align="center" underline="always">
+                Sign Up here
+              </Link>
+            </Typography>
+          </React.Fragment>
+          <Form onSubmit={handleFormSubmit} subscription={{ submitting: true }} onChange={handleChange}>
+            {({ handleSubmit2, submitting }) => (
+              <form onSubmit={handleSubmit2} className={classes.form} noValidate>
+                <Field
+                  autoComplete="email"
+                  autoFocus
+                  component={InputTextField}
+                  disabled={submitting || sent}
+                  fullWidth
+                  label="Email"
+                  margin="normal"
+                  name="email"
+                  required
+                  size="large"
+                />
+                <Field
+                  fullWidth
+                  size="large"
+                  component={InputTextField}
+                  disabled={submitting || sent}
+                  required
+                  name="password"
+                  autoComplete="current-password"
+                  label="Password"
+                  type="password"
+                  margin="normal"
+                />
+                <FormSpy subscription={{ submitError: true }}>
+                  {({ submitError }) =>
+                    submitError ? (
+                      <FormFeedback className={classes.feedback} error>
+                        {submitError}
+                      </FormFeedback>
+                    ) : null
+                  }
+                </FormSpy>
+                <FormButton
+                  className={classes.button}
+                  disabled={submitting || sent}
+                  size="large"
+                  color="#white"
+                  fullWidth
+                >
+                  {submitting || sent ? 'In progress…' : 'Sign In'}
+                </FormButton>
+              </form>
+            )}
+          </Form>
+          <React.Fragment>
+            <Typography> . </Typography>
+            <Typography variant="body2" align="center">
+              {'Forgot your password? '}
+              <Link
+                style={{ color: '#2B3D5B' }}
+                component={RouterLink}
+                to="/ForgotPassword"
+                align="center"
+                underline="always"
               >
-                {submitting || sent ? 'In progress…' : 'Sign In'}
-              </FormButton>
-            </form>
-          )}
-        </Form>
-        <Typography align="center">
-          <Link underline="always" href="/forgot-password/">
-            Forgot password?
-          </Link>
-        </Typography>
+                Update
+              </Link>
+            </Typography>
+          </React.Fragment>
+        </div>
       </AppForm>
     </React.Fragment>
   );
