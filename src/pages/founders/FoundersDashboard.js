5f663e6e76b7df6229c0023f9f00049c62985fcc
Georgia Hunt <georgialhunt89@gmail.com>
2021-08-16 15:32:48 +0930

    Added styling to foundersdashboard

--- src/pages/founders/FoundersDashboard.js
+++ src/pages/founders/FoundersDashboard.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { Box, Grid, Container, Typography } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { withStyles } from '@material-ui/core/styles';
 // components
 // import Page from '../components/page/Page';
 import CreateNewBusiness from '../../components/dashboard/founder/CreateNewBusiness';
 import BusinessPage from '../../components/dashboard/founder/BusinessPage';
 import Messages from '../../components/dashboard/founder/Messages';
 import NewInvestors from '../../components/dashboard/founder/NewInvestors';
-import Reports from '../../components/dashboard/founder/Reports';
+import BlogPosts from '../../components/dashboard/founder/BlogPosts';
 import BusinessPageVisits from '../../components/dashboard/founder/BusinessPageVisits';
 // import BlogPosts from '../components/dashboard/founder/BlogPosts';
 // import TrafficBySites from '../components/dashboard/founder/TrafficBySites';
@@ -15,49 +17,75 @@ import BusinessPageVisits from '../../components/dashboard/founder/BusinessPageV
 
 // ----------------------------------------------------------------------
 
+const styles = (theme) => ({
+  '@global': {
+    body: {
+      backgroundImage: "url('/img/DashboardBackgroundImg')",
+      backgroundColor: '#CC7D51',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center center',
+      backgroundSize: 'cover',
+      backgroundAttachment: 'fixed',
+      height: '100%',
+    },
+    html: {
+      height: '100%',
+    },
+    '#componentWithId': {
+      height: '100%',
+    },
+  },
+});
+
 export default function FounderDashboard() {
   return (
     // <Page title="Founder Dashboard">
     <Container maxWidth="xl">
-      <Box sx={{ pb: 5 }}>
-        <Typography variant="h4">Hi, Welcome back</Typography>
-      </Box>
-      <Grid item xs={12} md={6} lg={8}>
-        <CreateNewBusiness />
-      </Grid>
-      <Typography align="center" variant="h4">
-        Business Stats
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6} md={3}>
-          <BusinessPage />
+      <div styles={{ styles }} />
+      <Fragment>
+        <CssBaseline />
+        <Box sx={{ pb: 5 }}>
+          <Typography align="center" variant="h4">
+            Hi, Welcome back
+          </Typography>
+        </Box>
+        <Grid align="center">
+          <CreateNewBusiness />
         </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <NewInvestors />
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Messages />
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Reports />
-        </Grid>
+        <Typography align="center" variant="h4">
+          Business Stats
+        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={3}>
+            <BusinessPage />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <NewInvestors />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Messages />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <BlogPosts />
+          </Grid>
 
-        <Grid item xs={12} md={6} lg={8}>
-          <BusinessPageVisits />
-        </Grid>
+          <Grid item xs={12} md={6} lg={8}>
+            <BusinessPageVisits />
+          </Grid>
 
-        {/* <Grid item xs={12} md={6} lg={8}>
+          {/* <Grid item xs={12} md={6} lg={8}>
           <BlogPosts />
         </Grid> */}
 
-        {/* <Grid item xs={12} md={6} lg={4}>
+          {/* <Grid item xs={12} md={6} lg={4}>
           <TrafficBySites />
         </Grid> */}
 
-        {/* <Grid item xs={12} md={6} lg={8}>
+          {/* <Grid item xs={12} md={6} lg={8}>
           <Tasks />
         </Grid> */}
-      </Grid>
+        </Grid>
+      </Fragment>
     </Container>
     // </Page>
   );
