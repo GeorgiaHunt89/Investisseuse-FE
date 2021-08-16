1fbb59017b4ee6cd11554f3d90fbb32173d58f32
Georgia Hunt <georgialhunt89@gmail.com>
2021-08-16 15:33:08 +0930

    Cleaned Sign up page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { Form } from 'semantic-ui-react';
+// import { Form } from 'semantic-ui-react';
 import { Link } from '@material-ui/core/';
 import { useMutation } from '@apollo/client';
-import Auth from '../state/auth';
-import { ADD_USER } from '../api/mutations';
+import { MemoryRouter as Router } from 'react-router';
+import { Form, Field, FormSpy } from 'react-final-form';
+
+// ----------------------------------------------------------------------
+
+// Imports from Material Ui
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
@@ -12,27 +16,56 @@ import FormControl from '@material-ui/core/FormControl';
 import FilledInput from '@material-ui/core/FilledInput';
 import InputLabel from '@material-ui/core/InputLabel';
 import InputAdornment from '@material-ui/core/InputAdornment';
+import { Paper } from '@material-ui/core';
+import Checkbox from '@material-ui/core/Checkbox';
 
-import { MemoryRouter as Router } from 'react-router';
+// ----------------------------------------------------------------------
 
+// Components for this page
+import Auth from '../state/auth';
+import { ADD_USER } from '../api/mutations';
 import NavBar from '../components/navigation/NavBar';
 import AppForm from '../components/form/AppForm';
 import { email, required } from '../components/form/validate';
 import FormButton from '../components/button/FormButton';
 import Button from '../components/button/button';
 import FormFeedback from '../components/form/FormFeedback';
-import { Field, FormSpy } from 'react-final-form';
 import Typography from '../components/typography/Typography';
 import InputTextField from '../components/form/InputTextField';
 import Password from '../components/Password';
+import AppFooter from '../components/Footer';
+
+// Styling
+const LoginBackgroundImg = '../../img/Login-Background-Img.png';
 
 const useStyles = makeStyles((theme) => ({
+  background: {
+    backgroundImage: `url(${LoginBackgroundImg})`,
+    backgroundColor: '#D9B59D', // Beige
+    backgroundPosition: 'center',
+  },
+  root: {
+    backgroundImage: `url(${LoginBackgroundImg})`,
+    backgroundColor: '#Deba54',
+  },
   form: {
     marginTop: theme.spacing(6),
+    backgroundColor: '#D9B59D', // Beige
+    opacity: 0.95,
+    backgroundPosition: 'center',
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+    marginLeft: theme.spacing(12),
+    marginRight: theme.spacing(12),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    color: 'white',
   },
   button: {
+    backgroundColor: '#2B3D5B', // Navy
     marginTop: theme.spacing(3),
     marginBottom: theme.spacing(2),
+    color: 'white',
   },
   feedback: {
     marginTop: theme.spacing(2),
@@ -67,96 +100,91 @@ function Signup(props) {
   return (
     <React.Fragment>
       <NavBar />
-      <AppForm>
-        <React.Fragment>
-          <Typography variant="h3" gutterBottom marked="center" align="center">
-            Sign Up
-          </Typography>
-          <Typography variant="body2" align="center">
-            {'Already have an account? '}
-            <Link href="/Login" align="center" underline="always">
-              Log in here
-            </Link>
-          </Typography>
-        </React.Fragment>
-        <Form.Group Submit={handleFormSubmit} widths="equal">
-          <Grid container spacing={5}>
-            <Grid item xs={18} sm={12}>
-              <TextField
-                label="First Name"
-                id="firstName"
-                defaultValue=""
-                className={classes.textField}
-                margin="normal"
-                variant="filled"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                onChange={handleChange}
-              />
-            </Grid>
-          </Grid>
-          <Grid container spacing={5}>
-            <Grid item xs={18} sm={12}>
-              <TextField
-                label="Last Name"
-                id="lastName"
-                defaultValue=""
-                className={classes.textField}
-                margin="normal"
-                variant="filled"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                onChange={handleChange}
-              />
-            </Grid>
-          </Grid>
-        </Form.Group>
-        <Grid container spacing={5}>
-          <Grid item xs={18} sm={12}>
-            <Password />
-          </Grid>
-        </Grid>
-        <Grid container spacing={5}>
-          <Grid item xs={18} sm={12}>
-            <TextField
-              label="Email Address"
-              id="email"
-              defaultValue=""
-              className={classes.textField}
-              margin="normal"
-              variant="filled"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              onChange={handleChange}
-            />
-          </Grid>
-        </Grid>
-        <Grid container spacing={5}>
-          <Grid item xs={18} sm={12}>
-            <Form.Group margin="normal" variant="filled" />
-            <Typography variant="h8">Please select if you are a Founder</Typography>
-            <br />
-            <label margin="normal" variant="filled" className={classes.textField} for="founder">
-              Founder
-            </label>
-            <input type="checkbox" id="founder" name="founder" value={formState.founder} onChange={handleChange} />
-          </Grid>
-        </Grid>
-
-        <FormButton
-          to="/Profile"
-          type="submit"
-          className={classes.button}
-          color="secondary"
-          component={RouterLink}
-          fullWidth
-        >
-          Submit
-        </FormButton>
-      </AppForm>
+      <Paper>
+        <AppForm style={useStyles.LoginBackgroundImg}>
+          <React.Fragment>
+            <Typography variant="h3" gutterBottom align="center">
+              Sign Up
+            </Typography>
+            <Typography variant="body2" align="center">
+              {'Already have an account? '}
+              <Link style={{ color: '#Deba54' }} component={RouterLink} to="/Login" align="center" underline="always">
+                Log in here
+              </Link>
+            </Typography>
+          </React.Fragment>
+          <Form onSubmit={handleFormSubmit} subscription={{ submitting: true }} onChange={handleChange}>
+            {({ handleSubmit2, submitting }) => (
+              <form onSubmit={handleSubmit2} className={classes.form} noValidate>
+                <Field
+                  autoComplete="firstName"
+                  autoFocus
+                  component={InputTextField}
+                  disabled={submitting || sent}
+                  fullWidth
+                  label="First Name"
+                  margin="normal"
+                  name="firstName"
+                  required
+                  size="large"
+                />
+                <Field
+                  fullWidth
+                  size="large"
+                  component={InputTextField}
+                  disabled={submitting || sent}
+                  required
+                  name="lastName"
+                  autoComplete="lastName"
+                  label="Last Name"
+                  type="lastName"
+                  margin="normal"
+                />
+                <Field
+                  autoComplete="email"
+                  autoFocus
+                  component={InputTextField}
+                  disabled={submitting || sent}
+                  fullWidth
+                  label="Email"
+                  margin="normal"
+                  name="email"
+                  required
+                  size="large"
+                />
+                <Typography style={{ size: 'small' }}>Please select if you are a Founder</Typography>
+                <label margin="normal" variant="filled" className={classes.textField} for="founder">
+                  Founder
+                </label>
+                <input type="checkbox" id="founder" name="founder" value={formState.founder} onChange={handleChange} />
+                <FormSpy subscription={{ submitError: true }}>
+                  {({ submitError }) =>
+                    submitError ? (
+                      <FormFeedback className={classes.feedback} error>
+                        {submitError}
+                      </FormFeedback>
+                    ) : null
+                  }
+                </FormSpy>
+                <Grid container spacing={5}>
+                  <Grid item xs={18} sm={12}>
+                    <FormButton
+                      className={classes.button}
+                      disabled={submitting || sent}
+                      size="large"
+                      color="#white"
+                      fullWidth
+                    >
+                      {submitting || sent ? 'In progress…' : 'Sign In'}
+                    </FormButton>
+                  </Grid>
+                </Grid>
+              </form>
+            )}
+          </Form>
+        </AppForm>
+      </Paper>
+      <AppFooter />
     </React.Fragment>
   );
 }
