99357df9ef28c2a111d8a53ff34e2a911e3645ef
Georgia Hunt <georgialhunt89@gmail.com>
2021-08-16 15:36:48 +0930

    Updated signup mutation with Boolean

--- src/api/mutations.js
+++ src/api/mutations.js
@@ -11,6 +11,21 @@ export const LOGIN = gql`
   }
 `;
 
+export const SIGNUP = gql`
+  mutation signup($firstName: String!, $lastName: String!, $email: String!, $password: String!, $role: Boolean!) {
+    addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password, role: $role) {
+      token
+      user {
+        _id
+        firstName
+        lastName
+        email
+        role
+      }
+    }
+  }
+`;
+
 export const ADD_USER = gql`
   mutation addUser($firstName: String!, $lastName: String!, $email: String!, $password: String!, $role: Boolean!) {
     addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password, role: $role) {
@@ -80,3 +95,77 @@ export const ADD_BUSINESS = gql`
     }
   }
 `;
+
+export const UPDATE_BUSINESS = gql`
+  mutation updateBusiness(
+    $companyName: String!
+    $description: String!
+    $website: String!
+    $logo: String!
+    $sharePrice: Int!
+    $shareQuantity: Int!
+    $pitchDeck: String!
+    $category: String!
+    $owner: User!
+  ) {
+    addBusiness(
+      companyName: $companyName
+      description: $description
+      website: $website
+      logo: $logo
+      sharePrice: $sharePrice
+      shareQuantity: $shareQuantity
+      pitchDeck: $pitchDeck
+      category: $category
+    )
+    business {
+      _id
+      companyName
+      description
+      website
+      logo
+      sharePrice
+      shareQuantity
+      pitchDeck
+      category
+      owner {
+        _id
+        firstName
+      }
+    }
+  }
+`;
+
+// export const ADD_INVESTMENT = gql`
+//   mutation addInvestment( $shareNumber: Int!) {
+//     shareNumber
+//     investor {
+//       _id
+//       firstName
+//       lastName
+//       email
+//     }
+//     business {
+//       _id
+//       companyName
+//     }
+//   }
+//   }
+//   `;
+
+// export const UPDATE_INVESTMENT = gql`
+// mutation updateInvestment( $shareNumber: Int!){
+//   shareNumber
+//   investor{
+//     _id
+//     firstName
+//     lastName
+//     email
+//   }
+//   business {
+//     _id
+//     companyName
+//   }
+// }
+// }
+// `;
